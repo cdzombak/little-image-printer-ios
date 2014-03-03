7069cf278f75a958d91c442bd70b7db0c7a17351
Chris Dzombak <dzombak@nytimes.com>
2014-03-02 21:09:36 -0500

    Set up app styles

--- Little Image Printer/DPZAppDelegate.m
+++ Little Image Printer/DPZAppDelegate.m
@@ -13,26 +13,47 @@
 
 @interface DPZAppDelegate ()
 
-@property (nonatomic, strong) Reachability *reachability;
+@property (nonatomic, readonly) Reachability *reachability;
 
 @end
 
 @implementation DPZAppDelegate
 
+@synthesize reachability = _reachability;
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.reachability = [Reachability reachabilityWithHostname:@"remote.bergcloud.com"];    
-
     NSAssert([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone, @"Only iPhone is supported.");
     
     DPZViewController *vc = [[DPZViewController alloc] initWithNibName:@"DPZViewController_iPhone" bundle:nil];
     UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:vc];
+    
+    [self setupStyles];
 
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.rootViewController = navController;
     [self.window makeKeyAndVisible];
     
     return YES;
+}
+
+#pragma mark - App Setup
+
+- (void)setupStyles
+{
+    [[UINavigationBar appearance] setBarStyle:UIBarStyleBlack];
+    [[UINavigationBar appearance] setBarTintColor:[UIColor blackColor]];
+    [[UINavigationBar appearance] setTintColor:[UIColor colorWithRed:0.824 green:0.337 blue:0.071 alpha:1]];
+    [[UIBarButtonItem appearance] setTintColor:[UIColor colorWithRed:0.824 green:0.337 blue:0.071 alpha:1]];
+}
+
+#pragma mark - Reachability
+
+- (Reachability *)reachability {
+    if (!_reachability) {
+        _reachability = [Reachability reachabilityWithHostname:@"remote.bergcloud.com"];
+    }
+    return _reachability;
 }
 
 - (BOOL)isCloudReachable
